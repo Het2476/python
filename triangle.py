125f9adc38b6f8c9b1a31b0b566312e2849f3f77
Het2476 <prajapatihet024@gmail.com>
2025-02-25 15:28:59 +0530

    Delete triangle.py

--- triangle.py
+++
@@ -1,7 +0,0 @@
-a=int(input("enter 1st angle"))
-b=int(input("enter 2nd angle"))
-c=int(input("enter 3rd angle"))
-if((a+b+c)==180):
-    print("it is triangle")
-else:
-    print("it is not a triangle")
