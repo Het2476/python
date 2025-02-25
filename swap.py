6f9482cadd5e00c691a39628743ee0caae0985a8
Het2476 <prajapatihet024@gmail.com>
2025-02-25 15:29:21 +0530

    Delete swap.py

--- swap.py
+++
@@ -1,5 +0,0 @@
-a = int(input("Enter the value for the first number: "))
-b = int(input("Enter the value for the second number: "))
-print("Before swapping a and b: ",a,b)
-a,b = b,a
-print("After swapping a and b: ",a ,b)
